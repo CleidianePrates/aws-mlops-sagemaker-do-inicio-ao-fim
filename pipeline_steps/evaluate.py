0443ad99e1a62d49c15899d6a6b04edd8032cb07
Cleidiane Prates <c-leidiane2009@hotmail.com>
2024-10-23 21:15:22 -0300

    Update doc

--- pipeline_steps/evaluate.py
+++ pipeline_steps/evaluate.py
@@ -9,33 +9,62 @@ import tarfile
 import pickle as pkl
 import boto3
 
-# função auxiliar para carregar o modelo XGBoost em xgboost.Booster
 def load_model(model_data_s3_uri):
+    """
+    Carrega um modelo XGBoost a partir de um arquivo .tar.gz armazenado no S3.
+
+    Args:
+        model_data_s3_uri (str): URI S3 do arquivo do modelo.
+
+    Returns:
+        xgboost.Booster: Modelo XGBoost carregado.
+    """
+    # Define o nome do arquivo local para o modelo
     model_file = "./xgboost-model.tar.gz"
+    
+    # Extrai o nome do bucket e a chave do URI S3
     bucket, key = model_data_s3_uri.replace("s3://", "").split("/", 1)
+    
+    # Baixa o arquivo do modelo do S3
     boto3.client("s3").download_file(bucket, key, model_file)
     
+    # Extrai o conteúdo do arquivo tar.gz
     with tarfile.open(model_file, "r:gz") as t:
         t.extractall(path=".")
     
-    # Carregar modelo
+    # Carrega o modelo XGBoost
     model = xgb.Booster()
     model.load_model("xgboost-model")
 
     return model
 
 def plot_roc_curve(fpr, tpr):
+    """
+    Plota a curva ROC e salva como uma imagem.
+
+    Args:
+        fpr (array): Taxa de falsos positivos.
+        tpr (array): Taxa de verdadeiros positivos.
+
+    Returns:
+        str: Nome do arquivo da imagem salva.
+    """
+    # Define o nome do arquivo de saída
     fn = "roc-curve.png"
+    
+    # Cria uma nova figura
     fig = plt.figure(figsize=(6, 4))
     
-    # Plotar a linha diagonal 50%
+    # Plota a linha diagonal 50%
     plt.plot([0, 1], [0, 1], 'k--')
     
-    # Plotar o FPR e TPR alcançados pelo nosso modelo
+    # Plota o FPR e TPR alcançados pelo modelo
     plt.plot(fpr, tpr)
     plt.xlabel('Taxa de Falsos Positivos')
     plt.ylabel('Taxa de Verdadeiros Positivos')
     plt.title('Curva ROC')
+    
+    # Salva a figura
     plt.savefig(fn)
 
     return fn
@@ -50,23 +79,50 @@ def evaluate(
     pipeline_run_id=None,
     run_id=None,
 ):
+    """
+    Avalia um modelo XGBoost usando dados de teste e registra os resultados no MLflow.
+
+    Args:
+        test_x_data_s3_path (str): Caminho S3 para os dados de teste (features).
+        test_y_data_s3_path (str): Caminho S3 para os dados de teste (labels).
+        model_s3_path (str): Caminho S3 para o modelo treinado.
+        output_s3_prefix (str): Prefixo S3 para armazenar os resultados.
+        tracking_server_arn (str): ARN do servidor de rastreamento MLflow.
+        experiment_name (str, opcional): Nome do experimento MLflow.
+        pipeline_run_id (str, opcional): ID da execução do pipeline MLflow.
+        run_id (str, opcional): ID da execução MLflow.
+
+    Returns:
+        dict: Resultados da avaliação e informações relacionadas.
+    """
     try:
+        # Gera um sufixo único baseado no tempo atual
         suffix = strftime('%d-%H-%M-%S', gmtime())
+        
+        # Configura o servidor de rastreamento MLflow
         mlflow.set_tracking_uri(tracking_server_arn)
+        
+        # Configura ou cria um experimento MLflow
         experiment = mlflow.set_experiment(experiment_name=experiment_name if experiment_name else f"{evaluate.__name__ }-{suffix}")
+        
+        # Inicia uma execução de pipeline MLflow, se fornecido um ID
         pipeline_run = mlflow.start_run(run_id=pipeline_run_id) if pipeline_run_id else None            
+        
+        # Inicia uma execução MLflow para esta avaliação
         run = mlflow.start_run(run_id=run_id) if run_id else mlflow.start_run(run_name=f"evaluate-{suffix}", nested=True)
         
-        # Ler dados de teste
+        # Carrega os dados de teste
         X_test = xgb.DMatrix(pd.read_csv(test_x_data_s3_path, header=None).values)
         y_test = pd.read_csv(test_y_data_s3_path, header=None).to_numpy()
     
-        # Executar previsões
+        # Carrega o modelo e faz previsões
         probability = load_model(model_s3_path).predict(X_test)
     
-        # Avaliar previsões
+        # Calcula a curva ROC e o score AUC
         fpr, tpr, thresholds = roc_curve(y_test, probability)
         auc_score = auc(fpr, tpr)
+        
+        # Prepara os resultados da avaliação
         eval_result = {"evaluation_result": {
             "classification_metrics": {
                 "auc_score": {
@@ -75,43 +131,33 @@ def evaluate(
             },
         }}
         
+        # Registra a métrica AUC no MLflow
         mlflow.log_metric("auc_score", auc_score)
+        
+        # Plota e registra a curva ROC no MLflow
         mlflow.log_artifact(plot_roc_curve(fpr, tpr))
         
+        # Define o caminho S3 para a linha de base de previsão
         prediction_baseline_s3_path = f"{output_s3_prefix}/prediction_baseline/prediction_baseline.csv"
     
-        # Salvar arquivo de linha de base de previsão - precisaremos dele posteriormente para o monitoramento da qualidade do modelo
-        pd.DataFrame({"prediction":np.array(np.round(probability), dtype=int),
-                      "probability":probability,
-                      "label":y_test.squeeze()}
-                    ).to_csv(prediction_baseline_s3_path, index=False, header=True)
+        # Salva a linha de base de previsão no S3
+        pd.DataFrame({
+            "prediction": np.array(np.round(probability), dtype=int),
+            "probability": probability,
+            "label": y_test.squeeze()
+        }).to_csv(prediction_baseline_s3_path, index=False, header=True)
         
+        # Retorna os resultados e informações relacionadas
         return {
             **eval_result,
-            "prediction_baseline_data":prediction_baseline_s3_path,
-            "experiment_name":experiment.name,
-            "pipeline_run_id":pipeline_run.info.run_id if pipeline_run else ''
+            "prediction_baseline_data": prediction_baseline_s3_path,
+            "experiment_name": experiment.name,
+            "pipeline_run_id": pipeline_run.info.run_id if pipeline_run else ''
         }
             
     except Exception as e:
         print(f"Exceção no script de processamento: {e}")
         raise e
     finally:
+        # Finaliza a execução MLflow
         mlflow.end_run()
-
-
-# Este código é usado para avaliar um modelo XGBoost e registrar métricas e artefatos no MLflow. 
-# Aqui está o que cada função faz:
-
-# - `load_model`: Esta função carrega um modelo XGBoost serializado a partir de um URI S3.
-# - `plot_roc_curve`: Esta função plota a curva ROC (Receiver Operating Characteristic) e salva o gráfico 
-# como um artefato PNG.
-# - `evaluate`: Esta é a função principal que avalia o modelo XGBoost usando os dados de teste 
-# fornecidos. Ela calcula a pontuação AUC (Area Under the Curve) e registra a métrica e o gráfico da curva ROC no MLflow. Além disso, 
-# ela salva as previsões e probabilidades em um arquivo CSV chamado `prediction_baseline.csv` no prefixo S3 de saída fornecido. 
-# O resultado da avaliação, incluindo a pontuação AUC, o caminho do arquivo de linha de base de previsão, o nome do experimento e 
-# o ID da execução do pipeline, é retornado como um dicionário.
-
-# O código usa as bibliotecas `numpy`, `pandas`, `xgboost`, `mlflow`, `sklearn`, `matplotlib`, `tarfile`, `pickle` e `boto3` para 
-# carregar os dados, fazer previsões, avaliar o modelo, plotar a curva ROC e interagir com o MLflow e o Amazon S3.
-
